c92026d253bc35a44e31212cf1be213f213edd34
stefania <est.stefania.trianar@unimilitar.edu.co>
2026-04-10 19:50:15 -0500

    Agrega viewport y reinicio de puntos y correcciones

--- scrips.js
+++ scrips.js
@@ -3,11 +3,21 @@ const ctx = canvas.getContext("2d");
 
 let puntos = [];
 
-canvas.addEventListener("click", (e) => {
-    const x = e.offsetX
-    const y = e.offsetY
+// viewport
+const xmin = 100, ymin = 100;
+const xmax = 500, ymax = 300;
 
-    puntos.push({x, y});
+canvas.addEventListener("click", (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    if (puntos.length === 2) {
+        puntos = [];
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dibujarViewport();
+    }
+
+    puntos.push({ x, y });
 
     dibujarPunto(x, y);
 
@@ -16,17 +26,26 @@ canvas.addEventListener("click", (e) => {
     }
 });
 
+function dibujarViewport() {
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(xmin, ymin, xmax - xmin, ymax - ymin);
+}
+
+dibujarViewport();
+
 function dibujarPunto(x, y) {
-    ctx.fillStyle = "blue"
-    ctx.beginPath()
-    ctx.arc(x, y, 5, 0, Math.PI * 2)
-    ctx.fill()
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.fillText(`(${x},${y})`, x + 5, y - 5);
 }
 
 function dibujarLinea(p1, p2) {
-    ctx.strokeStyle = "red"
-    ctx.beginPath()
-    ctx.moveTo(p1.x, p1.y)
-    ctx.lineTo(p2.x, p2.y)
-    ctx.stroke()
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
 }
