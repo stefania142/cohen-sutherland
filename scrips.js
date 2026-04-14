937df3743ee07b5ad2823a8102d2e8d77edf258e
stefania <est.stefania.trianar@unimilitar.edu.co>
2026-04-14 14:41:22 -0500

    implementacion cohen-sutherland

--- scrips.js
+++ scrips.js
@@ -49,3 +49,52 @@ function dibujarLinea(p1, p2) {
     ctx.lineTo(p2.x, p2.y);
     ctx.stroke();
 }
+function getCode(x, y) {
+    let code = 0;
+    if (x < xmin) code |= 1; // izquierda
+    if (x > xmax) code |= 2; // derecha
+    if (y < ymin) code |= 4; // arriba
+    if (y > ymax) code |= 8; // abajo
+    return code;
+}   
+function cohenSutherland(p1, p2) {
+    let x1 = p1.x, y1 = p1.y;
+    let x2 = p2.x, y2 = p2.y;
+    let code1 = getCode(x1, y1);
+    let code2 = getCode(x2, y2);
+
+    while (true) {
+        if ((code1 & code2) === 0){
+            return { x1, y1, x2, y2 }; // ambos puntos dentro
+        } 
+        else if (code1 & code2) {
+            return null;
+        }
+        let codeOut = code1 !== 0 ? code1 : code2;
+        let x, y;
+
+        if (codeOut & 8) { // abajo
+            x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+            y = ymax;
+        } else if (codeOut & 4) { // arriba
+            x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+            y = ymin;
+        } else if (codeOut & 2) { // derecha
+            y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+            x = xmax;
+        } else if (codeOut & 1) { // izquierda
+            y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+            x = xmin;
+        }   
+        if (codeOut === code1) {
+            x1 = x;
+            y1 = y;
+            code1 = getCode(x1, y1);
+        }
+        else {
+            x2 = x;
+            y2 = y;
+            code2 = getCode(x2, y2);
+        }
+    }
+}
